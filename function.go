b2275874f0d4e97bd5f233c3d03ee8d28e8987d9
Alireza Bashiri <azbshiri@gmail.com>
2018-04-13 23:33:18 +0430

    Add redundant comment

--- function.go
+++ function.go
@@ -1,7 +1,6 @@
 package ramda
 
-// Always returns a function that always returns the given value. Note that for
-// non-primitives the value returned is a reference to the original value.
+// Always returns a function that always returns the given value.
 func Always(a interface{}) func() interface{} {
 	return func() interface{} {
 		return a
